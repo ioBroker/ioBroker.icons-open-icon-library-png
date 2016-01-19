369c585c71cc8222fa15f195cdf22600d5f5b5f8
Jey-Cee <christoph@von-gerle.de>
2016-01-19 23:46:24 +0100

    Update for new vis Structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,88 +8,151 @@
 module.exports = function (grunt) {
    // Project configuration.
 
+   var srcDir    = __dirname + '/';
+    var dstDir    = srcDir + '.build/';
+    var pkg       = grunt.file.readJSON('package.json');
+    var iopackage = grunt.file.readJSON('io-package.json');
+    var version   = (pkg && pkg.version) ? pkg.version : iopackage.common.version;
+
+    // Project configuration.
     grunt.initConfig({
-
-    });
-
-    grunt.registerTask('updateList', function () {
-        var fs = require('fs');
-        var dir = fs.readdirSync(__dirname + '/www');
-        var readme = '';
-        var html = '<html><body style="background: lightgray"><table>';
-        var inLine = 6;
-        for (var i = 0; i < dir.length; i++) {
-            if (dir[i] == 'index.html') continue;
-            var subdir = fs.readdirSync(__dirname + '/www/' + dir[i]);
-            var htmlLineImg  = '<tr>';
-            var htmlLineName = '<tr>';
-            html   += '<tr style="height:15px;background:lightblue"><td colspan="' + inLine + '" style="height:15px;font-size:24px;text-align:center">' + dir[i].substring(0, 1).toUpperCase() + dir[i].substring(1) + '</td></tr>';
-            readme += '### ' + dir[i] + '\n===========================\n';
-
-            var cur = 0;
-            for (var j = 0; j < subdir.length; j++) {
-				if (fs.statSync(__dirname + '/www/' + dir[i] + '/' + subdir[j]).isDirectory()) {
-					html += htmlLineImg + '</tr>';
-					html += htmlLineName + '</tr>';
-					htmlLineImg  = '<tr>';
-					htmlLineName = '<tr>';
-					var subsubdir = fs.readdirSync(__dirname + '/www/' + dir[i] + '/' + subdir[j]);
-					html   += '<tr style="height:15px;background:lightblue"><td colspan="' + inLine + '" style="height:15px;font-size:24px;text-align:center">' + subdir[j].substring(0, 1).toUpperCase() + subdir[j].substring(1) + '</td></tr>';
-					readme += '### ' + dir[i] + '/' + subdir[j] + '\n===========================\n';
-					var _cur = 0;
-					for (var k = 0; k < subsubdir.length; k++) {
-						if (_cur && !(_cur % inLine)) {
-							html += htmlLineImg  + '</tr>';
-							html += htmlLineName + '</tr>';
-							htmlLineImg  = '<tr>';
-							htmlLineName = '<tr>';
-							_cur = 0;
-						}
-						readme += '![' + subsubdir[k] + '](www/' + dir[i] + '/' + subdir[j] + '/' + subsubdir[k] + ')\n';
-
-						htmlLineImg  += '<td style="text-align: center"><img src="' + dir[i] + '/' + subdir[j] + '/' + subsubdir[k] + '" width="64" height="64"></td>\n';
-						if (subsubdir[k].length > 30) {
-							htmlLineName += '<td style="text-align: center" title="' + subsubdir[k] + '">' + subsubdir[k].substring(0, 30) + '...</td>\n';
-						} else {
-							htmlLineName += '<td style="text-align: center">' + subsubdir[k] + '</td>\n';
-						}
-						_cur++;
-					}
-					html += htmlLineImg + '</tr>';
-					html += htmlLineName + '</tr>';
-					htmlLineImg  = '<tr>';
-					htmlLineName = '<tr>';
-					continue;
-				}
-			
-				
-                if (cur && !(cur % inLine)) {
-                    html += htmlLineImg  + '</tr>';
-                    html += htmlLineName + '</tr>';
-                    htmlLineImg  = '<tr>';
-                    htmlLineName = '<tr>';
-                    cur = 0;
-                }
-                readme += '![' + subdir[i] + '](www/' + dir[i] + '/' + subdir[j] + ')\n';
-
-                htmlLineImg  += '<td style="text-align: center"><img src="' + dir[i] + '/' + subdir[j] + '" width="64" height="64"></td>\n';
-                if (subdir[j].length > 30) {
-                    htmlLineName += '<td style="text-align: center" title="' + subdir[j] + '">' + subdir[j].substring(0, 30) + '...</td>\n';
-                } else {
-                    htmlLineName += '<td style="text-align: center">' + subdir[j] + '</td>\n';
-                }
-                cur++;
+        pkg: pkg,
+        clean: {
+            all: ['tmp/*.json', 'tmp/*.zip', 'tmp/*.jpg', 'tmp/*.jpeg', 'tmp/*.png',
+                  dstDir + '*.json', dstDir + '*.zip', dstDir + '*.jpg', dstDir + '*.jpeg', dstDir + '*.png']
+        },
+        replace: {
+            core: {
+                options: {
+                    patterns: [
+                        {
+                            match: /version: *"[\.0-9]*"/,
+                            replacement: 'version: "' + version + '"'
+                        },
+                        {
+                            match: /"version": *"[\.0-9]*",/g,
+                            replacement: '"version": "' + version + '",'
+                        },
+                        {
+                            match: /version: *"[\.0-9]*",/g,
+                            replacement: 'version: "' + version + '",'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand:  true,
+                        flatten: true,
+                        src:     [
+                                srcDir + 'package.json',
+                                srcDir + 'io-package.json'
+                        ],
+                        dest:    srcDir
+                    },
+                    {
+                        expand:  true,
+                        flatten: true,
+                        src:     [
+                                srcDir + 'widgets/' + pkg.name.substring('iobroker.vis-'.length) + '.html'
+                        ],
+                        dest:    srcDir + 'widgets'
+                    },
+                ]
             }
-            html += htmlLineImg + '</tr>';
-            html += htmlLineName + '</tr>';
+        },
+        // Javascript code styler
+        jscs:   require(__dirname + '/tasks/jscs.js'),
+        // Lint
+        jshint: require(__dirname + '/tasks/jshint.js'),
+        http: {
+            get_hjscs: {
+                options: {
+                    url: 'https://raw.githubusercontent.com/ioBroker/ioBroker.js-controller/master/tasks/jscs.js'
+                },
+                dest: 'tasks/jscs.js'
+            },
+            get_jshint: {
+                options: {
+                    url: 'https://raw.githubusercontent.com/ioBroker/ioBroker.js-controller/master/tasks/jshint.js'
+                },
+                dest: 'tasks/jshint.js'
+            },/*
+            get_gruntfile: {
+                options: {
+                    url: 'https://raw.githubusercontent.com/ioBroker/ioBroker.build/master/adapters/Gruntfile.js'
+                },
+                dest: 'Gruntfile.js'
+            },
+            get_utilsfile: {
+                options: {
+                    url: 'https://raw.githubusercontent.com/ioBroker/ioBroker.build/master/adapters/utils.js'
+                },
+                dest: 'lib/utils.js'
+            },*/
+            get_jscsRules: {
+                options: {
+                    url: 'https://raw.githubusercontent.com/ioBroker/ioBroker.js-controller/master/tasks/jscsRules.js'
+                },
+                dest: 'tasks/jscsRules.js'
+            }
         }
-        html += '</table></body></html>';
-        grunt.file.write('ICONLIST.md', readme);
-        grunt.file.write('www/index.html', html);
     });
 
+    grunt.registerTask('updateReadme', function () {
+        var readme = grunt.file.read('README.md');
+        var pos = readme.indexOf('## Changelog\r\n');
+        if (pos != -1) {
+            var readmeStart = readme.substring(0, pos + '## Changelog\r\n'.length);
+            var readmeEnd   = readme.substring(pos + '## Changelog\r\n'.length);
+
+            if (iopackage.common && readme.indexOf(iopackage.common.version) == -1) {
+                var timestamp = new Date();
+                var date = timestamp.getFullYear() + '-' +
+                    ("0" + (timestamp.getMonth() + 1).toString(10)).slice(-2) + '-' +
+                    ("0" + (timestamp.getDate()).toString(10)).slice(-2);
+
+                var news = "";
+                if (iopackage.common.whatsNew) {
+                    for (var i = 0; i < iopackage.common.whatsNew.length; i++) {
+                        if (typeof iopackage.common.whatsNew[i] == 'string') {
+                            news += '* ' + iopackage.common.whatsNew[i] + '\r\n';
+                        } else {
+                            news += '* ' + iopackage.common.whatsNew[i].en + '\r\n';
+                        }
+                    }
+                }
+
+                grunt.file.write('README.md', readmeStart + '### ' + iopackage.common.version + ' (' + date + ')\r\n' + (news ? news + '\r\n\r\n' : '\r\n') + readmeEnd);
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jscs');
+    grunt.loadNpmTasks('grunt-http');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     grunt.registerTask('default', [
-        'updateList'
+        'exec',
+        'http',
+        'clean',
+        'replace',
+        'updateReadme',
+        'compress',
+        'copy',
+        'jshint',
+        'jscs'
+    ]);
+    grunt.registerTask('prepublish', [
+        'http',
+        'replace'
+    ]);
+	grunt.registerTask('p', [
+        'http',
+        'replace'
     ]);
 };
